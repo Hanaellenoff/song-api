876e216ba7680b26c8cf648efc933f7fdf066127
Hanaellenoff <hanaellenoff@gmail.com>
2024-02-29 14:14:09 -0600

    Add show action

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   get "/songs" => "songs#index"
   post "/songs" => "songs#create" 
+  get "/songs/:id" => "songs#show" 
   # Defines the root path route ("/")
   # root "posts#index"
 end
